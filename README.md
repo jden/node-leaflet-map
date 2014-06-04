edcf353b05f670ced0c20b2ce529a19a45898634
jden <jason@denizac.org>
2014-06-04 16:13:38 -0400

    impl

--- README.md
+++ README.md
@@ -3,12 +3,35 @@ simple way to make a leaflet map application
 
 ## usage
 ```js
+var L = require('leaflet')
 var leafletMap = require('leaflet-map')
+
+var map = leafletMap()
+L.tileLayer('https://{s}.tiles.mapbox.com/v3/examples.map-i86knfo3/{z}/{x}/{y}.png')
+  .addTo(map)
+
+L.marker([0,0])
+  .bindPopup('Null Island!')
+  .addTo(map)
+```
+
+Use with [browserify](https://npm.im/browserify) for best results!
+
+## run the example
+from the project root,
+```console
+$ npm run example
 ```
 
 
 ## api
+### `leafletMap(opt?: Object) => LeafletMap`
 
+creates a DOM `Element` and initializes a Leaflet map in it.
+
+`opt` is an object with any of the following properties:
+
+- `defaultStyle` boolean (default: `true`). Whether to include CSS and set basic page styling to render a full-page map.
 
 ## installation
 
--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-function leafletMap () {
-  
+var L = require('leaflet')
+
+function leafletMap (opt) {
+  opt = opt || {
+    defaultStyle: true
+  }
+  if (opt.defaultStyle) {
+    L.Icon.Default.imagePath = 'http://cdn.leafletjs.com/leaflet-0.7.3/images'
+    ensureStyle()
+  }
+
+  var mapEl = document.createElement('div')
+  mapEl.setAttribute('style','width:100%;height:100%;position:inherit;')
+  document.body.appendChild(mapEl)
+
+  return L.map(mapEl)
+}
+
+function ensureStyle() {
+  document.documentElement.style.height = '100%'
+  document.body.style.height = '100%'
+  document.body.style.margin = '0'
+
+  var mapStyle = document.createElement('link')
+  mapStyle.setAttribute('rel','stylesheet')
+  mapStyle.setAttribute('href','http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css')
+  document.body.appendChild(mapStyle)
 }
 
 module.exports = leafletMap
